d1cfff1b1464bda7d28aa53594252f3f6b074bc0
Sebastian Schuberth <sschuberth@gmail.com>
2020-12-29 17:41:44 +0100

    Support cataloging from the "Recent" directory

--- src/recentitems.py
+++ src/recentitems.py
@@ -2,7 +2,11 @@
 
 import keypirinha as kp
 import keypirinha_util as kpu
-import keypirinha_net as kpnet
+from keypirinha_wintypes import FOLDERID
+
+import os.path
+import time
+
 
 class RecentItems(kp.Plugin):
     """
@@ -31,26 +35,77 @@ class RecentItems(kp.Plugin):
 
     More detailed documentation at: http://keypirinha.com/api/plugin.html
     """
+
+    CONFIG_SECTION_MAIN = "main"
+
+    DEFAULT_SCAN_RECENT_DIRECTORY = True
+
+    scan_recent_directory = DEFAULT_SCAN_RECENT_DIRECTORY
+
     def __init__(self):
         super().__init__()
 
     def on_start(self):
-        pass
+        self._read_config()
 
     def on_catalog(self):
-        pass
+        catalog = []
 
-    def on_suggest(self, user_input, items_chain):
-        pass
+        if self.scan_recent_directory:
+            start = time.perf_counter()
+
+            self._add_from_recent_directory(catalog)
+
+            self._log_catalog_duration(start, "link", len(catalog))
+
+        self.set_catalog(catalog)
 
     def on_execute(self, item, action):
-        pass
-
-    def on_activated(self):
-        pass
-
-    def on_deactivated(self):
-        pass
+        kpu.execute_default_action(self, item, action)
 
     def on_events(self, flags):
-        pass
+        if flags & kp.Events.PACKCONFIG:
+            if self._read_config():
+                self.on_catalog()
+
+    def _read_config(self):
+        new_scan_recent_directory = self.load_settings().get_bool(
+            "scan_recent_directory", self.CONFIG_SECTION_MAIN, self.DEFAULT_SCAN_RECENT_DIRECTORY
+        )
+
+        config_changed = new_scan_recent_directory != self.scan_recent_directory
+        self.scan_recent_directory = new_scan_recent_directory
+
+        return config_changed
+
+    def _add_from_recent_directory(self, catalog):
+        recent_directory = os.path.join(
+            kpu.shell_known_folder_path(FOLDERID.RoamingAppData.value),
+            "Microsoft", "Windows", "Recent"
+        )
+
+        try:
+            recent_links = kpu.scan_directory(
+                recent_directory, "*.lnk",
+                kpu.ScanFlags.FILES,
+                max_level=0
+            )
+        except OSError as e:
+            self.dbg(str(e))
+            return
+
+        for link_name in recent_links:
+            link_path = os.path.join(recent_directory, link_name)
+            item = self.create_item(
+                category=kp.ItemCategory.FILE,
+                label=os.path.splitext(link_name)[0],
+                short_desc="Recent link: " + link_path,
+                target=link_path,
+                args_hint=kp.ItemArgsHint.ACCEPTED,
+                hit_hint=kp.ItemHitHint.KEEPALL
+            )
+            catalog.append(item)
+
+    def _log_catalog_duration(self, start, what, count):
+        elapsed = time.perf_counter() - start
+        self.info("Cataloged {} {}{} in {:.1f} seconds".format(count, what, "s"[count == 1:], elapsed))
